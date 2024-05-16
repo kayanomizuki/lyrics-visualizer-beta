bc56a2ea92363d12e97ae57eb58233a2a49982ad
kayanomizuki <dev.kayanomizuki@gmail.com>
2024-05-17 04:54:53 +0900

    update: change referrerPolicy

--- src/pages/api/musixmatch/lyrics.ts
+++ src/pages/api/musixmatch/lyrics.ts
@@ -18,7 +18,7 @@ export default async function handler(
         {
           method: "GET",
           referrer: `${ROOT}`,
-          referrerPolicy: "no-referrer-when-downgrade",
+          referrerPolicy: "strict-origin",
         }
       )
         .then((response) => {
